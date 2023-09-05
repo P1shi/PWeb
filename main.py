ca0cda6122c16e060d703d1ab32ae11ad82bfc75
P1shi <138011257+P1shi@users.noreply.github.com>
2023-09-05 23:11:07 +0330

    Reworked & Fixed 👾

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
+from colorama import Fore, Style, init
 import time
 import threading
-from os import system
+import platform
+import os
 
 B = '\033[35m'
 R = '\033[31m'
@@ -9,63 +11,71 @@ N = '\033[0m'
 A = '\033[34m'
 BB = '\033[36m'
 
-system("cls")
-
-while True:
-    print(R + """
-
-
-
-   ▄███████▄  ▄█     █▄     ▄████████ ▀█████████▄  
-  ███    ███ ███     ███   ███    ███   ███    ███ 
-  ███    ███ ███     ███   ███    █▀    ███    ███ 
-  ███    ███ ███     ███  ▄███▄▄▄      ▄███▄▄▄██▀  
-▀█████████▀  ███     ███ ▀▀███▀▀▀     ▀▀███▀▀▀██▄  
-  ███        ███     ███   ███    █▄    ███    ██▄ 
-  ███        ███ ▄█▄ ███   ███    ███   ███    ███ 
- ▄████▀       ▀███▀███▀    ██████████ ▄█████████▀  
-                                                   
-╭—————————————————————————————————————————————————╮
-|Tool devoloped : P1shi                           |
-|Github         : https://github.com/P1shi        |
-|Discord        : Pishi  ᵁⱽ#1027                  |
-╰—————————————————————————————————————————————————╯
+def main():
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+    print(f"""
+{R}██████{B}╗ {R}██{B}╗    {R}██{B}╗{R}███████{B}╗{R}██████{B}╗ 
+{R}██{B}╔══{R}██{B}╗{R}██{B}║    {R}██{B}║{R}██{B}╔════╝{R}██{B}╔══{R}██{B}╗
+{R}██████{B}╔╝{R}██{B}║ {R}█{B}╗ {R}██{B}║{R}█████{B}╗  {R}██████{B}╔╝
+{R}██{B}╔═══╝ {R}██{B}║{R}███{B}╗{R}██{B}║{R}██{B}╔══╝  {R}██{B}╔══{R}██{B}╗
+{R}██{B}║     {B}╚{R}███{B}╔{R}███{B}╔╝{R}███████{B}╗{R}██████{B}╔╝
+╚═╝      ╚══╝╚══╝ ╚══════╝╚═════╝ 
+                                  
 """)
-    webhook_url = input(R + "[" + B + "#" + R + "] Webhook URL : ")
-    custom_username = input(R + "[" + B + "+" + R + "] " + "Webhook username : ")
-    avatar_url = " "
-    message_content = input(R + "[" + B + "+" + R + "] " + "Content : ")
-    messages_per_sec = input(R + "[" + B + "+" + R + "] " + "Messages per second : ")
-    if not messages_per_sec.isdigit():
-        print(R + "[" + B + "!" + R + "] " + "Please put a known number in the blank." )
-        time.sleep(2)
-        print("\033c")
-        continue
-    messages_per_sec = int(messages_per_sec)
-    message = {"content": message_content, "username": custom_username, "avatar_url": avatar_url}
+    try:
+        webhook_url = input(f"{R}[{B}+{R}] Webhook URL : {B}")
+        print(f"{B}[{A}${B}] Cheking Webhook...")
+        time.sleep(1)
+        def check_webhook_existence(webhook_url):
+            try:
+                response = requests.head(webhook_url)
+                if response.status_code == 200:
+                    return True
+                else:
+                    return False
+            except requests.exceptions.RequestException as e:
+                return False
+
+        if check_webhook_existence(webhook_url):
+            print(f"{B}[{A}~{B}] Webhook Exists.")
+        else:
+            print(f"{R}[{B}!{R}] Webhook does not exist.")
+            time.sleep(5)
+            main()
+        custom_username = input(f"{R}[{B}+{R}] Webhook username : {B}")
+        avatar_url = ""
+        threads_use = input(f"{R}[{B}+{R}] Threads : {B}")
+        if not threads_use.isdigit():
+            print(f"{R}[{B}!{R}] Please Enter a Valid Number")
+            time.sleep(5)
+            main()
+        message_content = input(f"{R}[{B}+{R}] Content : {B}")
+        message = {"content": message_content, "username": custom_username, "avatar_url": avatar_url}
+
+    except:
+        print(f"{R}[{B}!{R}] An Unknown Error Occurred.\n\nTip : Maybe You Leave Some Input Blanks")
 
     def send_messages():
         while True:
-            for i in range(messages_per_sec):
+            for i in range(50):
+                
                 response = requests.post(webhook_url, json=message)
                 if response.status_code == 204:
-                    print(A + "[" + BB + "$" + A + "] " + BB + "1 message successfully sent" )
+                    print(f"{B}[{A}~{B}] {A}» {B} Message Successfully Sent.")
                 elif response.status_code == 429:
-                    print(R + "[" + B + "!" + R + "] " + "You are being rate limited " )
-                
+                    print(f"{R}[{B}!{R}] {B}» {R}You are being rate limited.")
+                    time.sleep(7)
+                    response = requests.post(webhook_url, json=message)
                 elif response.status_code == 404:
-                    print(R + "[" + B + "!" + R + "] " + "Invalid webhook url " )
-                    time.sleep(4.0)
-                    print("\033c")
-                    break
-            else:
-                time.sleep(1)
-                continue
-            break
+                    print(f"{R}[{B}!{R}] {B}» {R}Cant Fing The Webhook.")
+            time.sleep(2)
 
 
     threads = []
-    for i in range(messages_per_sec):  
+    for i in range(3):  
         t = threading.Thread(target=send_messages)
         threads.append(t)
         t.start()
@@ -75,7 +85,5 @@ while True:
         t.join()
 
 
-    print(R + "[" + B + "!" + R + "] " + " an unknown error occurred\nplease check webhook url or other things.\n Note : remember that webhook url , username , message content and messages per sec are important but webhook profile is optional" )
-    time.sleep(3)
-    print("\033c")
-
+    
+main()
